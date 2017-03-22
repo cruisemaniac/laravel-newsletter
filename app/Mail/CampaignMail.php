29762917bfc0f7df82e83a5d1ee08bd02f16a51c
Ashwin Murali <mail@cruisemaniac.com>
2017-03-22 11:02:33 +0530

    this is how it works for me at the moment… so i’m leaving it this way…

--- app/Mail/CampaignMail.php
+++ app/Mail/CampaignMail.php
@@ -35,16 +35,23 @@ class CampaignMail extends Mailable implements ShouldQueue
      */
     public function build()
     {
-        return $this->subject( env('SITE_NAME').": ". $this->campaign->subject)->view('emails.campaign')->with([
-            'content' => $this->str_replace_dynamic([
-                '%subject%' => $this->campaign->subject,
-                '%email%' => $this->subscription->email,
-                '%content%' => $this->campaign->content,
-                '%name%' => $this->subscription->name,
-                '%country%' => countries($this->subscription->country),
-                '%unsubscribe_link%' => route('subscriptions.preunsubscribe', [$this->subscription->email, $this->subscription->unsubscribe])
-            ], $this->template->content)
-        ]);
+        $final_array = [];
+        $final_array['%subject%'] = $this->campaign->subject;
+        $final_array['%email%'] = $this->subscription->email;
+        $final_array['%content%'] = $this->campaign->content;
+        $final_array['%unsubscribe_link%'] = route('subscriptions.preunsubscribe', [$this->subscription->email, $this->subscription->unsubscribe]);
+
+        $content = str_replace(array_keys($final_array), array_values($final_array), $this->template->content);
+
+        return $this->subject( env('SITE_NAME').": ". $this->campaign->subject)->view('emails.campaign')->with(['content' => $content]);
+        // return $this->subject( env('SITE_NAME').": ". $this->campaign->subject)->view('emails.campaign')->with([
+        //     'content' => $this->str_replace_dynamic([
+        //         '%subject%' => $this->campaign->subject,
+        //         '%email%' => $this->subscription->email,
+        //         '%content%' => $this->campaign->content,
+        //         '%unsubscribe_link%' => route('subscriptions.preunsubscribe', [$this->subscription->email, $this->subscription->unsubscribe])
+        //     ], $this->template->content)
+        // ]);
     }
 
     public function str_replace_dynamic(array $replace, $string)
